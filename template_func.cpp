7bf018944ecb56a62cb61a170c3c00b0a1e01086
= <ashu1461@gmail.com>
2013-05-11 16:46:15 +0530

    using pairs, functional lib, dp

--- template_func.cpp
+++ template_func.cpp
@@ -1,9 +1,9 @@
 /*
-Sonakshi Nathani <sonakshinathani@gmail.com>
+   Sonakshi Nathani <sonakshinathani@gmail.com>
 information: 
 #tags: template_func
 date: Sun May  5 20:59:37 IST 2013
-*/
+ */
 #include<iostream>
 using namespace std;
 template <class T>
@@ -14,13 +14,13 @@ T GetMax (T a,T b) {
 }
 
 int main() {
-int i=5, j=6, k;
-string l = "sonakshi";
-string m = "sonaksho",n;
-k = GetMax<int>(i,j);
-n = GetMax<string>(l,m);
-cout << k << endl;
-cout << n << endl;
-return 0;
+    int i=5, j=6, k;
+    string l = "sonakshi";
+    string m = "sonaksho",n;
+    k = GetMax<int>(i,j);
+    n = GetMax<string>(l,m);
+    cout << k << endl;
+    cout << n << endl;
+    return 0;
 }
 
